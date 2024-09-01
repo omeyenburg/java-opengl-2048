54f63cf44f8311aa25c02e7b4c99ae4720381b26
os-mey <oskar.meyenburg@gmail.com>
2024-09-01 22:08:22 +0200

    Create Shader.java

---
+++ app/src/main/java/org/main/graphics/Shader.java
@@ -0,0 +1,11 @@
+package org.main.graphics;
+
+import org.lwjgl.opengl.*;
+import static org.lwjgl.opengl.GL30.*;
+
+public class Shader {
+    public static void create(String[] vertex, String[] fragment) {
+        int program = GL20.glCreateProgram();
+
+    }
+}
